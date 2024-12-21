f9a579e7e3ef39466371950b5a4ed9787ce6f498
Dillon Jayatillake <dillonmichaeljay@gmail.com>
2024-12-21 15:44:57 +0530

    Update FileSaver.js

--- home/FileSaver.js
+++ home/FileSaver.js
@@ -49,3 +49,13 @@ function download(url, name, opts) {
     }
     xhr.send()
 }
+
+function corsEnabled(url) {
+    var xhr = new XMLHttpRequest()
+    // use sync to avoid popup blocker
+    xhr.open('HEAD', url, false)
+    try {
+        xhr.send()
+    } catch (e) {}
+    return xhr.status >= 200 && xhr.status <= 299
+}
